0bd2e24391bd6da07a916d4f56ac3f617b5a407c
Noelle Huchette <noelle.huchette@gmail.com>
2023-01-27 17:08:50 -0500

    queery

--- pages/article/[id].js
+++ pages/article/[id].js
@@ -3,17 +3,34 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+export async function getServerSideProps(context) {
+  const { id } = context.params;
+  const article = await prisma.articles.findUnique({
+    where: {
+      id: id
+    }
+  });
+  return {
+    props: {
+      article: JSON.parse(
+        JSON.stringify(
+          article,
+          (key, value) => (typeof value === 'bigint' ? value.toString() : value) // return everything else unchanged
+        )
+      )
+    }
+  };
+}
+
 export default function Article(props) {
+  console.log(props.article);
   const router = useRouter();
   const { id } = router.query;
 
   return (
     <>
       <h1>{id}</h1>
+      {props.article.title}
     </>
   );
-}
-
-export async function getServerSideProps(context) {
-  return { props: {} };
 }
