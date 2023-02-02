dc838c0e502390d95062ef182bd17fc14822e6b2
Noelle Huchette <noelle.huchette@gmail.com>
2023-02-02 15:34:49 -0500

    getting set up

--- pages/_app.js
+++ pages/_app.js
@@ -5,17 +5,28 @@ import createEmotionCache from '../util/createEmotionCache';
 import { CacheProvider } from '@emotion/react';
 import { Footer } from '@/components/Footer';
 import Navbar from '@/components/Navbar';
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+import { useState } from 'react';
+
 const clientSideEmotionCache = createEmotionCache();
 
 function App({ Component, emotionCache = clientSideEmotionCache, pageProps }) {
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+
   return (
-    <CacheProvider value={emotionCache}>
-      <ThemeProvider theme={theme}>
-        <Navbar />
-        <Component {...pageProps} />
-        <Footer />
-      </ThemeProvider>
-    </CacheProvider>
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <CacheProvider value={emotionCache}>
+        <ThemeProvider theme={theme}>
+          <Navbar />
+          <Component {...pageProps} />
+          <Footer />
+        </ThemeProvider>
+      </CacheProvider>
+    </SessionContextProvider>
   );
 }
 export default App;
