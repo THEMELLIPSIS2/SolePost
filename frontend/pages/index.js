7e85877da6580a16c9e22ad76dfe14ce7530b24c
Fiona Murray <fiona.c.murray@gmail.com>
2023-02-22 22:32:51 -0500

    uninstalled

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -5,46 +5,19 @@ import styles from '@/styles/Home.module.css';
 import '@/styles/Homepage.module.css'
 import shoe from '../public/shoe.svg';
 import {HomePage} from '../components/Home'
+import StrapiClient from '@/lib/strapi-client';
 
+const client = new StrapiClient()
 
+export const getStaticProps = async () => {
+  const articles = await client.fetchData('/articles')
+  return {
+    props: {
+      articles: articles
+    }
+  }
+}
 
-
-// export async function getServerSideProps() {
-//     // let recents = await prisma.articles.findMany({
-//     //     where: {
-//     //         published: true,
-//     //         featured: false
-//     //     },
-//     //     orderBy: {
-//     //         created_at: 'desc'
-//     //     },
-//     //     take: 3
-//     //   })
-//     // let features = await prisma.articles.findMany({
-//     //   where: {
-//     //     published:true,
-//     //     featured:true
-//     //   },
-//     //   orderBy: {
-//     //     created_at: 'desc'
-//     //   },
-//     //   take:3
-//     // })
-//     // return {
-//     //   props: {
-//     //     recents: JSON.parse(JSON.stringify(recents, (key, value) =>
-//     //     typeof value === 'bigint'
-//     //         ? value.toString()
-//     //         : value // return everything else unchanged
-//     // )),
-//     //     features: JSON.parse(JSON.stringify(features, (key, value) =>
-//     //     typeof value === 'bigint'
-//     //         ? value.toString()
-//     //         : value // return everything else unchanged
-//     // )),
-//     //   }
-//     // };
-//   }
 
 
 export default function Home({recents=[],features=[]}) {
