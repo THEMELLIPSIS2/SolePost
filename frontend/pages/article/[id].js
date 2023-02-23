e08b56cdda453fac64a1962decb4eef9c4128c15
Fiona Murray <fiona.c.murray@gmail.com>
2023-02-22 21:23:54 -0500

    lots of moving and deleting

---
+++ frontend/pages/article/[id].js
@@ -0,0 +1,33 @@
+import { useRouter } from 'next/router';
+
+
+// export async function getServerSideProps(context) {
+//   // const { id } = context.params;
+//   // const article = await prisma.articles.findUnique({
+//   //   where: {
+//   //     id: id
+//   //   }
+//   // });
+//   // return {
+//   //   props: {
+//   //     article: JSON.parse(
+//   //       JSON.stringify(
+//   //         article,
+//   //         (key, value) => (typeof value === 'bigint' ? value.toString() : value) // return everything else unchanged
+//   //       )
+//   //     )
+//   //   }
+//   // };
+// }
+
+export default function Article(props={}) {
+  const router = useRouter();
+  const { id } = router.query;
+
+  return (
+    <>
+      <h1>{id}</h1>
+      {props.article.title}
+    </>
+  );
+}
